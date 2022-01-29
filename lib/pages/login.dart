78613ad95bbd8ae21d3cdb4d4c505143034afd67
Alexander Park <parkalex380@gmail.com>
2022-01-29 16:01:28 -0600

    try anonymous auth

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  void _anonymouslogin() async {
+    Firebase.initializeApp();
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInAnonymously();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(
-            child: Text('LoginScreen', textDirection: TextDirection.ltr)));
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text('LoginScreen', textDirection: TextDirection.ltr),
+      TextButton(onPressed: _anonymouslogin, child: Text("anonymous gogin"))
+    ]));
   }
 }
