e94745c1c8b6295943792778e7b7cb47116b3e34
Kevin <ge.kevin.a@gmail.com>
2022-01-29 17:55:27 -0600

    Setup fb init

---
+++ lib/pages/loading_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<double> rotateAnimation;
+
+  @override
+  void initState() {
+    // Animations
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 2500),
+      vsync: this,
+    );
+    rotateAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.elasticInOut,
+    );
+
+    animationController.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(60),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.25),
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          // we need a loading widget
+          RotationTransition(turns: rotateAnimation, child: Text('test')),
+        ],
+      ),
+    );
+  }
+}
