50653f567723b8bbcb078b85be944ca1de334892
Eli Nicksic <elinicksic@gmail.com>
2022-01-29 21:51:33 -0600

    Update readme and title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class App extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Planer',
       theme: ThemeData(
         // This is the theme of your application.
         //
